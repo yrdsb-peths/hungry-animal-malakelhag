61392703c80b184c756a7ab4b502624f5efaac78
malakelhag <150281047+malakelhag@users.noreply.github.com>
2023-11-23 10:01:50 -0500

    organized objects

--- MyWorld.java
+++ MyWorld.java
@@ -9,20 +9,24 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         kangaro kangaro = new kangaro();
-        addObject(kangaro, 150, 200);
+        addObject(kangaro, 300, 300);
         prepare();
     }
+    
+
+    public void spawnFries()
+    {
+        fries fries = new fries();
+        addObject(fries, 300, 100);
+    }
+    
     
     public void act()
     {
         spawnFries();
     }
-
-    public void spawnFries()
-    {
-        fries fries = new fries();
-        addObject(fries, 150, 200);
-    }
+    
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
