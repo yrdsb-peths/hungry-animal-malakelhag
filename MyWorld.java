f44927f5db08b9ca5dcf9955159861a1e30b044e
malakelhag <150281047+malakelhag@users.noreply.github.com>
2023-12-07 00:33:03 -0500

    added a new score label

--- MyWorld.java
+++ MyWorld.java
@@ -3,6 +3,7 @@ import greenfoot.*;
 public class MyWorld extends World {
     private int score = 0;
     private int friesEatenCount = 0;
+    private int highestScore = 0; // New variable to store the highest score
     private Label scoreLabel;
 
     public MyWorld() {    
@@ -16,15 +17,30 @@ public class MyWorld extends World {
     }
 
     public void onGameOver() {
-        Label gameOverLabel = new Label("Game Over!", 70);
+        Label gameOverLabel = new Label("Game Over", 50);
         addObject(gameOverLabel, getWidth() / 2, getHeight() / 2);
+
+        // Display the score and check for a new record
+        String message = "Score: " + score;
+        if (score > highestScore) {
+            highestScore = score;
+            message += "\nNew Record!";
+        }
+        Label scoreMessageLabel = new Label(message, 30);
+        addObject(scoreMessageLabel, getWidth() / 2, getHeight() / 2 + 50);
+
+        // Reset the score for the next game
+        score = 0;
+        friesEatenCount = 0;
+        Greenfoot.delay(100); // Delay before restarting the game
+        Greenfoot.setWorld(new MyWorld());
     }
 
     public void increaseScore() {
         score = score + 1;
         scoreLabel.setValue(score);
 
-        if (++friesEatenCount % 1 == 0) {
+        if (++friesEatenCount % 5 == 0) {
             for (Object obj : getObjects(Fries.class)) {
                 Fries fries = (Fries) obj;
                 fries.setActInterval(fries.getActInterval() - 1);
