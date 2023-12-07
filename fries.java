e6617b40210f47fdbc084e9610a2d64da566f886
malakelhag <150281047+malakelhag@users.noreply.github.com>
2023-12-07 02:37:26 -0500

    saved fries file

--- fries.java
+++ fries.java
@@ -1,39 +1,30 @@
 import greenfoot.*;
-
-public class Fries extends Actor {
+public class Fries extends Actor 
+{
     private int actInterval = 5; // Initial act interval
     private int actCounter = 0;
-
     public Fries() {
         GreenfootImage image = getImage();
         image.scale(40, 40);
-        setImage(image);
     }
-
     public void act() {
         if (actCounter % actInterval == 0) {
             setLocation(getX(), getY() + 5);
         }
-
         actCounter++;
-
         checkCollision();
     }
-
     public void setActInterval(int interval) {
         actInterval = interval;
     }
-
     public int getActInterval() {
         return actInterval;
     }
-
     public void checkCollision() {
         setLocation(getX(), getY() + 2);
         MyWorld world = (MyWorld) getWorld();
         if (getY() > world.getHeight()) {
             world.onGameOver();
         }
-}
-
+    }
 }
