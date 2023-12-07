ef923d4de97a574022d3ae2d6fe9a9ea968d11b3
malakelhag <150281047+malakelhag@users.noreply.github.com>
2023-12-06 22:47:18 -0500

    tried to add an animation to the kangaro

--- kangaro.java
+++ kangaro.java
@@ -1,22 +1,43 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  
 
 public class Kangaro extends Actor
 {
     GreenfootSound sound = new GreenfootSound("sounds/Eating sound effect.mp3");
     
-    
+    GreenfootImage[] rightImages = new GreenfootImage[8];
+    GreenfootImage[] leftImages = new GreenfootImage[8];
+    int animationDelay = 100; // Set your desired delay here
+    int animationCounter = 0;
+    boolean facingRight = true;
+
     public Kangaro() {
-        GreenfootImage image = getImage();
-        image.scale(100, 100);
-        setImage(image);
+        for(int i = 0; i < rightImages.length; i++) {
+            rightImages[i] = new GreenfootImage("images/kangaroo.png");
+            rightImages[i].scale(100, 100);
+        }
+        for(int i = 0; i < leftImages.length; i++) {
+            leftImages[i] = new GreenfootImage("images/kangaroo.png");
+            leftImages[i].scale(100, 100);
+            leftImages[i].mirrorHorizontally();
+        }
+        setImage(rightImages[3]);
     }
-    
-    
-    
-    
+
+    public void animateKangaro() {
+        if (animationCounter % (animationDelay / 100) == 0) {
+            if(facingRight) {
+                setImage(rightImages[(animationCounter / (animationDelay / 100)) % rightImages.length]);
+            } else {
+                setImage(leftImages[(animationCounter / (animationDelay / 100)) % leftImages.length]);
+            }
+        }
+        animationCounter++;
+    }
     
     public void act()
     {
+        animateKangaro();
+        
         if(Greenfoot.isKeyDown("a")){
             move(-3);    
         }
@@ -37,6 +58,4 @@ public class Kangaro extends Actor
             world.increaseScore();
         }
     }
-    
-   
 }
