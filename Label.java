6c2edcf8376bf0c7eae06bc3a9e7907a24e65dfa
malakelhag <150281047+malakelhag@users.noreply.github.com>
2023-12-06 23:03:08 -0500

    Allowed the kangaro to jump

--- Label.java
+++ Label.java
@@ -83,3 +83,4 @@ public class Label extends Actor
         setImage(new GreenfootImage(value, fontSize, fillColor, transparent, lineColor));
     }
 }
+
--- kangaro.java
+++ kangaro.java
@@ -1,21 +1,22 @@
-import greenfoot.*;  
+import greenfoot.*;
 
-public class Kangaro extends Actor
-{
-    GreenfootSound sound = new GreenfootSound("sounds/Eating sound effect.mp3");
-    
+public class Kangaro extends Actor {
+    private boolean isJumping = false;
+
     GreenfootImage[] rightImages = new GreenfootImage[8];
     GreenfootImage[] leftImages = new GreenfootImage[8];
-    int animationDelay = 100; // Set your desired delay here
+    int animationDelay = 100;
     int animationCounter = 0;
     boolean facingRight = true;
 
+    GreenfootSound sound = new GreenfootSound("sounds/Eating sound effect.mp3");
+
     public Kangaro() {
-        for(int i = 0; i < rightImages.length; i++) {
+        for (int i = 0; i < rightImages.length; i++) {
             rightImages[i] = new GreenfootImage("images/kangaroo.png");
             rightImages[i].scale(100, 100);
         }
-        for(int i = 0; i < leftImages.length; i++) {
+        for (int i = 0; i < leftImages.length; i++) {
             leftImages[i] = new GreenfootImage("images/kangaroo.png");
             leftImages[i].scale(100, 100);
             leftImages[i].mirrorHorizontally();
@@ -25,7 +26,7 @@ public class Kangaro extends Actor
 
     public void animateKangaro() {
         if (animationCounter % (animationDelay / 100) == 0) {
-            if(facingRight) {
+            if (facingRight) {
                 setImage(rightImages[(animationCounter / (animationDelay / 100)) % rightImages.length]);
             } else {
                 setImage(leftImages[(animationCounter / (animationDelay / 100)) % leftImages.length]);
@@ -33,24 +34,38 @@ public class Kangaro extends Actor
         }
         animationCounter++;
     }
-    
-    public void act()
-    {
+
+    public void act() {
         animateKangaro();
-        
-        if(Greenfoot.isKeyDown("a")){
-            move(-3);    
+
+        if (Greenfoot.isKeyDown("a")) {
+            move(-3);
         }
-        if(Greenfoot.isKeyDown("d")){
+        if (Greenfoot.isKeyDown("d")) {
             move(3);
         }
-        
+        if (Greenfoot.isKeyDown("space") && !isJumping) {
+            jump();
+        }
+
         eat();
     }
-    
+
+    private void jump() {
+        isJumping = true;
+        for (int i = 0; i < 20; i++) {
+            setLocation(getX(), getY() - 5);
+            Greenfoot.delay(1);
+        }
+        for (int i = 0; i < 20; i++) {
+            setLocation(getX(), getY() + 5);
+            Greenfoot.delay(1);
+        }
+        isJumping = false;
+    }
+
     public void eat() {
-        // Eat the Fries
-        if (isTouching(Fries.class)){
+        if (isTouching(Fries.class)) {
             removeTouching(Fries.class);
             sound.play();
             MyWorld world = (MyWorld) getWorld();
