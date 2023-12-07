313a2038bcd1d2aaedce665e4e90bfd808c06013
malakelhag <150281047+malakelhag@users.noreply.github.com>
2023-12-07 00:41:49 -0500

    shows score when done, + press space to start again

--- MyWorld.java
+++ MyWorld.java
@@ -3,37 +3,35 @@ import greenfoot.*;
 public class MyWorld extends World {
     private int score = 0;
     private int friesEatenCount = 0;
-    private int highestScore = 0; // New variable to store the highest score
+    private int highestScore = 0;
     private Label scoreLabel;
+
+    private boolean gameOver = false;
 
     public MyWorld() {    
         super(600, 400, 1, false);
-        Kangaro kangaroo = new Kangaro();
-        addObject(kangaroo, 300, 300);
+        Kangaro kangaro = new Kangaro();
+        addObject(kangaro, 300, 300);
         spawnFries();
 
         scoreLabel = new Label(0, 70);
         addObject(scoreLabel, 50, 50);
     }
 
+    public void act() {
+        if (Greenfoot.isKeyDown("space") && gameOver) {
+            Greenfoot.setWorld(new MyWorld());
+        }
+    }
+
     public void onGameOver() {
+        gameOver = true;
         Label gameOverLabel = new Label("Game Over", 50);
         addObject(gameOverLabel, getWidth() / 2, getHeight() / 2);
 
-        // Display the score and check for a new record
-        String message = "Score: " + score;
-        if (score > highestScore) {
-            highestScore = score;
-            message += "\nNew Record!";
-        }
-        Label scoreMessageLabel = new Label(message, 30);
+        // Display the score
+        Label scoreMessageLabel = new Label("Score: " + score + "\nPress space to start again", 30);
         addObject(scoreMessageLabel, getWidth() / 2, getHeight() / 2 + 50);
-
-        // Reset the score for the next game
-        score = 0;
-        friesEatenCount = 0;
-        Greenfoot.delay(100); // Delay before restarting the game
-        Greenfoot.setWorld(new MyWorld());
     }
 
     public void increaseScore() {
