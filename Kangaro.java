139a4111dee4ae004ded818b1d334d970695ec40
DylanDinesh <350444048@gapps.yrdsb.ca>
2023-12-11 12:38:06 -0500

    Revert "Change sprite and added animations"
    This reverts commit f6b0bf96653f93e787b42daaea7815dc344ee1ff.

--- Kangaro.java
+++ Kangaro.java
@@ -3,54 +3,36 @@ import greenfoot.*;
 public class Kangaro extends Actor {
     private boolean isJumping = false;
 
-    GreenfootImage[] idleRight = new GreenfootImage[3];
-    GreenfootImage[] idleLeft = new GreenfootImage[3];
+    GreenfootImage[] rightImages = new GreenfootImage[8];
+    GreenfootImage[] leftImages = new GreenfootImage[8];
+    int animationDelay = 100;
+    int animationCounter = 0;
     boolean facingRight = true;
-    SimpleTimer animationTimer = new SimpleTimer();
+
     GreenfootSound sound = new GreenfootSound("sounds/Eating sound effect.mp3");
 
     public Kangaro() {
-        for (int i = 0; i < idleRight.length; i++)
-        {
-            idleRight[i] = new GreenfootImage("images/tile00" + i + ".png");
-            idleRight[i].scale(100, 100);
+        for (int i = 0; i < rightImages.length; i++) {
+            rightImages[i] = new GreenfootImage("images/kangaroo.png");
+            rightImages[i].scale(140, 140);
         }
-
-        for (int i = 0; i < idleLeft.length; i++)
-        {
-            idleLeft[i] = new GreenfootImage("images/tile00" + i + ".png");
-            idleLeft[i].mirrorHorizontally();
-            idleLeft[i].scale(100, 100);
+        for (int i = 0; i < leftImages.length; i++) {
+            leftImages[i] = new GreenfootImage("images/kangaroo.png");
+            leftImages[i].scale(100, 100);
+            leftImages[i].mirrorHorizontally();
         }
-
-        animationTimer.mark();
-        // Initial elephant image
-        setImage(idleRight[0]);
+        setImage(rightImages[3]);
     }
 
-
-    /**
-     * Animate the Kangaro
-     */
-    int imageIndex = 0;
-    public void animateKangaro()
-    {
-        if (animationTimer.millisElapsed() < 100)
-        {
-            return;
+    public void animateKangaro() {
+        if (animationCounter % (animationDelay / 100) == 0) {
+            if (facingRight) {
+                setImage(rightImages[(animationCounter / (animationDelay / 100)) % rightImages.length]);
+            } else {
+                setImage(leftImages[(animationCounter / (animationDelay / 100)) % leftImages.length]);
+            }
         }
-        animationTimer.mark();
-
-        if (facingRight == true)
-        {
-            setImage(idleRight[imageIndex]);
-            imageIndex = (imageIndex + 1) % idleRight.length;
-        }
-        else
-        {
-            setImage(idleLeft[imageIndex]);
-            imageIndex = (imageIndex + 1) % idleLeft.length;
-        }
+        animationCounter++;
     }
 
     public void act() {
@@ -58,11 +40,9 @@ public class Kangaro extends Actor {
 
         if (Greenfoot.isKeyDown("a")) {
             move(-4);
-            facingRight = false;
         }
         if (Greenfoot.isKeyDown("d")) {
             move(4);
-            facingRight = true;
         }
         if (Greenfoot.isKeyDown("j") && !isJumping) {
             jump();
